e98b0e0a3a1ee0452cf30409124bbe9a353650f3
Prakhar Mishra <pmprakhargenius@gmail.com>
2023-03-15 22:02:38 +0530

    Add files via upload

---
+++ decode_taskA_run1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+RUN="run1"
+mkdir -p outputs/$RUN
+mkdir -p intermediate_outputs/$RUN
+
+python3 src/01_taska_classification_traininingandinference.py $1 $RUN
+python3 src/02_taska_summarisation_trainingandinference.py $1 $RUN
+python src/03_taska_summarisationensemble.py $1 $RUN
+python src/04_taska_finaloutputgeneration.py $RUN
