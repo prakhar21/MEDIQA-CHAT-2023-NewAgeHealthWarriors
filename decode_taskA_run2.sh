0e308ccd6837162bb3f69e0e12c751968056f8a4
Prakhar Mishra <pmprakhargenius@gmail.com>
2023-03-16 09:31:11 +0530

    Update decode_taskA_run2.sh

--- decode_taskA_run2.sh
+++ decode_taskA_run2.sh
@@ -6,5 +6,5 @@ mkdir -p intermediate_outputs/$RUN
 
 python3 src/01_taska_classification_traininingandinference.py $1 $RUN
 python3 src/02_taska_summarisation_trainingandinference.py $1 $RUN
-python src/03_taska_summarisationensemble.py $1 $RUN
-python src/04_taska_finaloutputgeneration.py $RUN
+python3 src/03_taska_summarisationensemble.py $1 $RUN
+python3 src/04_taska_finaloutputgeneration.py $RUN
